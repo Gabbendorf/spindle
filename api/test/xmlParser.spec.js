1b6c869782f1b717c239915e205778c647ad2a4e
andrewMacmurray <a.macmurray@icloud.com>
2018-07-20 11:44:52 +0100

    Extracts parserStub, use async await in xmlParser test

--- api/test/xmlParser.spec.js
+++ api/test/xmlParser.spec.js
@@ -2,36 +2,29 @@ const { expect } = require("chai");
 const xmlParser = require("../src/authors/xmlParser.js");
 const sampleMediumResponse = require("./testData/mediumResponse.js");
 
+const parserStub = {
+  parseURL: () => Promise.resolve(sampleMediumResponse)
+};
+
 describe("xmlParser", () => {
   describe("parseFeed", () => {
-    it("parses an xml feed url into JSON", () => {
-      const parserStub = {
-        parseURL: () => Promise.resolve(sampleMediumResponse)
-      };
-
+    it("parses an xml feed url into JSON", async () => {
       const feedConfig = {
         source: "medium",
         author: "Gabi",
         url: "https://medium.com/feed/@gabriellamedas"
       };
 
-      xmlParser
-        .parseFeed(parserStub, feedConfig)
-        .then(response => {
-          expect(response.source).to.eq("medium");
-          expect(response.author).to.eq("Gabi");
-          expect(response.feed).to.deep.equal(sampleMediumResponse);
-        })
-        .catch(err => expect.fail(err));
+      const response = await xmlParser.parseFeed(parserStub, feedConfig)
+
+      expect(response.source).to.eq("medium");
+      expect(response.author).to.eq("Gabi");
+      expect(response.feed).to.deep.equal(sampleMediumResponse);
     });
   });
 
   describe("parseFeeds", () => {
     it("given an array of feed urls parses all into JSON", async () => {
-      const parserStub = {
-        parseURL: () => Promise.resolve(sampleMediumResponse)
-      };
-
       const feedConfigs = [
         {
           source: "medium",
@@ -46,7 +39,7 @@ describe("xmlParser", () => {
       ];
 
       const responses = await xmlParser.parseFeeds(parserStub, feedConfigs);
-      expect(responses.length).to.eq(2);
+
       responses.forEach(response => {
         expect(response.feed).to.deep.equal(sampleMediumResponse);
       });
