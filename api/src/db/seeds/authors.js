4bb4d112ccc3afda99833376e2f1b80361cab04e
andrewMacmurray <a.macmurray@icloud.com>
2018-08-06 19:49:04 +0100

    adds default seeds for authors

---
+++ api/src/db/seeds/authors.js
@@ -0,0 +1,72 @@
+const connectionConfig = require('../connectionConfig.js');
+const {Client} = require('pg');
+const authors = [
+  {
+    source: 'medium',
+    first_name: 'Gabriella',
+    second_name: 'Medas',
+    url: 'https://medium.com/feed/@gabriellamedas',
+  },
+  {
+    source: 'medium',
+    first_name: 'Andrew',
+    second_name: 'MacMurray',
+    url: 'https://medium.com/feed/@andrewMacmurray',
+  },
+  {
+    source: 'medium',
+    first_name: 'Katerina',
+    second_name: 'Georgiou',
+    url: 'https://medium.com/feed/@katerinacodes',
+  },
+  {
+    source: 'medium',
+    first_name: 'Himalee',
+    second_name: 'Tailor',
+    url: 'https://medium.com/feed/@himalee.tailor',
+  },
+  {
+    source: 'medium',
+    first_name: 'Devlin',
+    second_name: 'Glasman',
+    url: 'https://medium.com/feed/@devlin.glasman',
+  },
+  {
+    source: 'jekyll',
+    first_name: 'Laurent',
+    second_name: 'Curau',
+    url: 'https://laurentcurau.com/feed.xml',
+  },
+  {
+    source: 'medium',
+    first_name: 'Marion',
+    second_name: 'LV',
+    url: 'https://medium.com/feed/@codeidoscope'
+  }
+];
+
+const deleteExistingAuthorsQuery = `DELETE FROM authors`;
+const authorValue = a =>
+  `('${a.first_name}', '${a.second_name}', '${a.url}', '${a.source}')`;
+
+const authorsValues = authors.map(authorValue).join(',');
+
+const addAuthorsQuery = `
+  INSERT INTO authors (first_name, second_name, blog_url, blog_source)
+  VALUES ${authorsValues};
+`;
+
+async function seedAuthors() {
+  try {
+    await client.connect();
+    await client.query(deleteExistingAuthorsQuery);
+    await client.query(addAuthorsQuery);
+    await client.end();
+    console.log('added authors');
+  } catch (err) {
+    console.log(err);
+    await client.end();
+  }
+}
+
+seedAuthors();
